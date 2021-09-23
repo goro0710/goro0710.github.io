e4181b0b318ec6e383867841c3c188840ba94e50
Gaurav Sharma <gsharma48@dxc.com>
2021-09-23 09:07:08 +0530

    const to var

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { create, createReportList } from './modules/canvas.js';
 import { name, draw, reportArea, reportPerimeter } from './modules/square.js';
 import randomSquare from './modules/square.js';
 
-name = "Triangle";
+var name = "Triangle";
 
 let myCanvas = create('myCanvas', document.body, 480, 320);
 let reportList = createReportList(myCanvas.id);
