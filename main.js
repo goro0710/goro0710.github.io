c54c26f15a2bd1fe0a36aa660fae2e7a2f6c8032
Gaurav Sharma <gsharma48@dxc.com>
2021-09-24 08:39:17 +0530

    update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { create, createReportList } from './modules/canvas.js';
 import { Square } from './modules/square.js';
-import randomSquare from './modules/square.js';
 
 let myCanvas = create('myCanvas', document.body, 480, 320);
 let reportList = createReportList(myCanvas.id);
@@ -9,6 +8,3 @@ let square1 = new Square();
 Square.draw(myCanvas.ctx, 50, 50, 100, 'blue');
 Square.reportArea(square1.length, reportList);
 Square.reportPerimeter(square1.length, reportList);
-
-// Use the default
-let square2 = randomSquare(myCanvas.ctx);
