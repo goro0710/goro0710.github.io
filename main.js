4e09c608281e75a0d9fe2f32f8a00957e535a3a6
Gaurav Sharma <gsharma48@dxc.com>
2021-09-24 08:36:52 +0530

    using class as module export

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import { create, createReportList } from './modules/canvas.js';
-import * as Square from './modules/square.js';
+import { Square } from './modules/square.js';
 import randomSquare from './modules/square.js';
 
 let myCanvas = create('myCanvas', document.body, 480, 320);
 let reportList = createReportList(myCanvas.id);
 
-let square1 = Square.draw(myCanvas.ctx, 50, 50, 100, 'blue');
+let square1 = new Square();
+Square.draw(myCanvas.ctx, 50, 50, 100, 'blue');
 Square.reportArea(square1.length, reportList);
 Square.reportPerimeter(square1.length, reportList);
 
--- modules/square.js
+++ modules/square.js
@@ -1,57 +1,60 @@
-const name = 'square';
+export class Square{
+  constructor(){
+    
+  }
 
-function draw(ctx, length, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, length, length);
+  name = 'square';
 
-  return {
-    length: length,
-    x: x,
-    y: y,
-    color: color
-  };
+  draw(ctx, length, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, length, length);
+  
+    return {
+      length: length,
+      x: x,
+      y: y,
+      color: color
+    };
+  }
+  
+  random(min, max) {
+     let num = Math.floor(Math.random() * (max - min)) + min;
+     return num;
+  }
+  
+  reportArea(length, listId) {
+    let listItem = document.createElement('li');
+    listItem.textContent = `${name} area is ${length * length}px squared.`
+  
+    let list = document.getElementById(listId);
+    list.appendChild(listItem);
+  }
+  
+  reportPerimeter(length, listId) {
+    let listItem = document.createElement('li');
+    listItem.textContent = `${name} perimeter is ${length * 4}px.`
+  
+    let list = document.getElementById(listId);
+    list.appendChild(listItem);
+  }
+  
+  randomSquare(ctx) {
+    let color1 = random(0, 255);
+    let color2 = random(0, 255);
+    let color3 = random(0, 255);
+    let color = `rgb(${color1},${color2},${color3})`
+    ctx.fillStyle = color;
+  
+    let x = random(0, 480);
+    let y = random(0, 320);
+    let length = random(10, 100);
+    ctx.fillRect(x, y, length, length);
+  
+    return {
+      length: length,
+      x: x,
+      y: y,
+      color: color
+    };
+  }
 }
-
-function random(min, max) {
-   let num = Math.floor(Math.random() * (max - min)) + min;
-   return num;
-}
-
-function reportArea(length, listId) {
-  let listItem = document.createElement('li');
-  listItem.textContent = `${name} area is ${length * length}px squared.`
-
-  let list = document.getElementById(listId);
-  list.appendChild(listItem);
-}
-
-function reportPerimeter(length, listId) {
-  let listItem = document.createElement('li');
-  listItem.textContent = `${name} perimeter is ${length * 4}px.`
-
-  let list = document.getElementById(listId);
-  list.appendChild(listItem);
-}
-
-function randomSquare(ctx) {
-  let color1 = random(0, 255);
-  let color2 = random(0, 255);
-  let color3 = random(0, 255);
-  let color = `rgb(${color1},${color2},${color3})`
-  ctx.fillStyle = color;
-
-  let x = random(0, 480);
-  let y = random(0, 320);
-  let length = random(10, 100);
-  ctx.fillRect(x, y, length, length);
-
-  return {
-    length: length,
-    x: x,
-    y: y,
-    color: color
-  };
-}
-
-export { name, draw, reportArea, reportPerimeter };
-export default randomSquare;
