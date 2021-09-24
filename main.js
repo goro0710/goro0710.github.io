04d796a1a5c28cdc57d7e62c298c1a02e1a521cb
Gaurav Sharma <gsharma48@dxc.com>
2021-09-24 08:42:47 +0530

    defect

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ let myCanvas = create('myCanvas', document.body, 480, 320);
 let reportList = createReportList(myCanvas.id);
 
 let square1 = new Square();
-Square.draw(myCanvas.ctx, 50, 50, 100, 'blue');
-Square.reportArea(square1.length, reportList);
-Square.reportPerimeter(square1.length, reportList);
+square1.draw(myCanvas.ctx, 50, 50, 100, 'blue');
+square1.reportArea(square1.length, reportList);
+square1.reportPerimeter(square1.length, reportList);
