208b247ffe227563302251a557d34fc78806ba61
Gaurav Sharma <gsharma48@dxc.com>
2021-09-24 08:30:00 +0530

    using import as object

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import { create, createReportList } from './modules/canvas.js';
-import { name, draw as drawSquare, reportArea as reportSquareArea, reportPerimeter as reportSquarePerimeter} from './modules/square.js';
+import * as Square from './modules/square.js';
 import randomSquare from './modules/square.js';
 
 let myCanvas = create('myCanvas', document.body, 480, 320);
 let reportList = createReportList(myCanvas.id);
 
-let square1 = drawSquare(myCanvas.ctx, 50, 50, 100, 'blue');
-reportSquareArea(square1.length, reportList);
-reportSquarePerimeter(square1.length, reportList);
+let square1 = Square.draw(myCanvas.ctx, 50, 50, 100, 'blue');
+Square.reportArea(square1.length, reportList);
+Square.reportPerimeter(square1.length, reportList);
 
 // Use the default
 let square2 = randomSquare(myCanvas.ctx);
